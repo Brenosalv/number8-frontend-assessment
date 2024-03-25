9eca95ff49c81e3686940a7e8dd6202cd0d51fdc
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-03-25 15:30:11 -0300

    fix: solve the modal height overflowing screen height

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -12,10 +12,10 @@ export function Modal({ isOpen, onClose, title, children }: ModalProps) {
   if (!isOpen) return null
 
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none mx-4'>
-      <div className='relative w-auto max-w-md mx-auto my-6 z-50'>
-        <div className='relative bg-white'>
-          <div className='flex items-center justify-between gap-16 p-4 border-b border-solid border-gray-300'>
+    <div className='fixed inset-0 z-50 flex items-center justify-center outline-none focus:outline-none mx-4'>
+      <div className='relative w-auto max-w-md mx-auto z-50'>
+        <div className='relative bg-white flex flex-col h-modal-height'>
+          <div className='flex items-center justify-between p-4 border-b border-solid border-gray-300'>
             <h3 className='text-lg font-semibold'>{title}</h3>
             <button
               className='p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
@@ -26,7 +26,7 @@ export function Modal({ isOpen, onClose, title, children }: ModalProps) {
               </span>
             </button>
           </div>
-          <div className='relative p-6 flex-auto'>{children}</div>
+          <div className='p-6 flex-1 overflow-y-auto'>{children}</div>
         </div>
       </div>
       <div className='fixed inset-0 z-40 bg-black opacity-50'></div>
